b2e3e49e74f56d4181a3bc4141a595c6ab47e0b2
Bruce Eckel <mindviewinc@gmail.com>
2017-08-05 11:12:46 -0600

    Added all_urls()

--- git-all.py
+++ git-all.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 import click
 import subprocess
+import platform
 
 
 if 'GIT_HOME' not in os.environ:
@@ -26,6 +27,7 @@ def cli():
     Performs various commands on all repositories in your local directory
     """
 
+
 @cli.command()
 def pull():
     "Pull on all repos"
@@ -36,6 +38,23 @@ def pull():
 def status():
     "Get status of all repos"
     git_command("status -s")
+
+
+@cli.command()
+def all_urls():
+    "Produce urls of all repos on this machine"
+    urls = []
+    for gd in [x for x in Path(os.environ['GIT_HOME']).iterdir() if x.is_dir()]:
+        config = gd / ".git" / "config"
+        result = "No url found"
+        for line in [ln.strip() for ln in config.read_text().splitlines()]:
+            if line.startswith("url ="):
+                result = line.split()[-1]
+        urls.append(result)
+    repo_list = Path(__file__).parent / (platform.node() + "_repos.txt")
+    repo_list.write_text("\n".join(sorted(urls)) + "\n")
+    os.system(f"cat {repo_list}")
+
 
 if __name__ == '__main__':
     cli()
