e6f5b0e905b267bbf7ca8d46c1f90c15fe3aa56e
Bruce Eckel <mindviewinc@gmail.com>
2017-08-16 12:21:01 -0600

    Fix for new path and actions

--- x.bat
+++ x.bat
@@ -1,12 +1,5 @@
 @echo off
-cd %GIT_HOME%\AtomicKotlin
-rem if exist %GIT_HOME%\AtomicKotlin\ExtractedExamples (
-rem  bb code clean
-rem )
-if exist %GIT_HOME%\AtomicKotlin\ExtractedExamples (
-  cd %GIT_HOME%\AtomicKotlin\ExtractedExamples
-  rm -rf Examples
-)
-bb code extract
-cd %GIT_HOME%\AtomicKotlin\ExtractedExamples
-gradlew build
+cd %GIT_HOME%\ExtractedExamples
+call gradlew clean
+call bb code extract
+call gradlew build
