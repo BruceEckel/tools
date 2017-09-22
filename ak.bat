05af28212ebb99a6fad804896a867a8be1e94f7a
Bruce Eckel <mindviewinc@gmail.com>
2017-09-22 11:47:12 -0600

    Make more useful

--- ak.bat
+++ ak.bat
@@ -1,7 +1,4 @@
 @echo off
-if exist %GIT_HOME%\AtomicKotlin\ExtractedExamples (
-  cd %GIT_HOME%\AtomicKotlin\ExtractedExamples
-) else (
-  cd %GIT_HOME%\AtomicKotlin
-)
-%GIT_HOME%\BookBuilder\virtualenv\Scripts\activate.bat
+cd %GIT_HOME%\AtomicKotlin
+call %GIT_HOME%\BookBuilder\virtualenv\Scripts\activate.bat
+cd %GIT_HOME%\AtomicKotlinExtractedExamples\Examples
