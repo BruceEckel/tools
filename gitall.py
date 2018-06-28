9e06cfaba9ed62862f8632859402037731376f5f
Cody Bontecou <bontecouc@gmail.com>
2018-06-28 09:16:42 -0600

    added newline at end of file as well as changed skip list

--- gitall.py
+++ gitall.py
@@ -5,7 +5,7 @@ import click
 import subprocess
 import platform
 
-skip = ["AtomicKotlin", "AtomicKotlin-Edu", "OnJava8-Examples-master", "PracticeJava", "IntuitiveJava.github.io",
+skip = ["AtomicKotlin", "AtomicKotlin-Edu", "OnJava8-Examples-master", "IntuitiveJava.github.io",
         ".idea", ".git"]
 
 if 'GIT_HOME' not in os.environ:
