5724043b520f352cfd7b3ddb08cb1e46ff800810
Bruce Eckel <BruceTEckel@gmail.com>
2020-10-10 16:54:44 -0600

    Update kotlin-exercises.py
    Added timestamp checker

--- kotlin-exercises.py
+++ kotlin-exercises.py
@@ -1,23 +1,16 @@
-import pprint
-import sys
-from pathlib import Path
 import subprocess
+from pathlib import Path
 
+edit_boundary = 1602369729.1761289  # Oct 10, 2020
 
 exercises_dir = Path("c:/", "Git", "AtomicKotlinCourse")
 
 tasks = list(exercises_dir.rglob("task.md"))
 
-
-def exists(*paths: Path):
-    for path in paths:
-        if not path.exists():
-            print(f"{path} does not exist")
-            sys.exit(1)
-
-
 if __name__ == '__main__':
     for task in tasks:
         if 'Examples' in task.parts:
+            continue
+        if task.stat().st_mtime > edit_boundary:
             continue
         subprocess.call(['subl.exe', f"{task}"])
