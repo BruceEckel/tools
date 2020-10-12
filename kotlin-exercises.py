e16fe3e34f6b5d0764400c4cff75939af74672f6
Bruce Eckel <BruceTEckel@gmail.com>
2020-10-12 10:40:21 -0600

    Update kotlin-exercises.py

--- kotlin-exercises.py
+++ kotlin-exercises.py
@@ -1,6 +1,8 @@
 import subprocess
 from pathlib import Path
 
+# Once complete, capitalize IDEA everywhere
+
 edit_boundary = 1602369729.1761289  # Oct 10, 2020
 
 exercises_dir = Path("c:/", "Git", "AtomicKotlinCourse")
@@ -11,6 +13,8 @@ if __name__ == '__main__':
     for task in tasks:
         if 'Examples' in task.parts:
             continue
-        if task.stat().st_mtime > edit_boundary:
-            continue
-        subprocess.call(['subl.exe', f"{task}"])
+        # if task.stat().st_mtime > edit_boundary:
+        #     continue
+        text = task.read_text()
+        if "idea" in text or "Idea" in text:
+            subprocess.call(['subl.exe', f"{task}"])
