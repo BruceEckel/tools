b02ae03c1016b646dce7dd31cdc90a3b2b11629a
Bruce Eckel <mindviewinc@gmail.com>
2023-10-27 16:18:09 -0600

    Update console.ps1

--- console.ps1
+++ console.ps1
@@ -1,3 +1,3 @@
 # Shortcut command:
 # powershell.exe -ExecutionPolicy Bypass -F C:\git\tools\console.ps1
-wt --title Python --tabColor '#009999' -d C:\git\python-experiments\ `; new-tab --title Rust --tabColor '#f59218' -d C:\git\rust-experiments\
+wt --title Python --tabColor '#009999' -d C:\git\python-experiments\ `; new-tab --title Rust --tabColor '#f59218' -d C:\git\rust-experiments\ `; new-tab --title Go --tabColor '#f59218' -d C:\git\go-experiments\
