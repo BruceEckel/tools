66f9da51fea47018d3b2b4b28bbafa15017198d6
Bruce Eckel <mindviewinc@gmail.com>
2024-11-15 18:20:15 -0700

    Update console.ps1

--- console.ps1
+++ console.ps1
@@ -2,4 +2,4 @@
 # powershell.exe -ExecutionPolicy Bypass -F C:\git\tools\console.ps1
 # --tabColor '#f59218' 
 # wt --title "lazy_guide code" --tabColor '#009999' -d C:\git\lazy_guide `; new-tab --title "Lazy Guide Chapters" --tabColor '#f59218' -d C:\git\LazyGuide\chapters\ `;
-wt --tabColor '#f59218' -d C:\git\LazyGuide\chapters `; new-tab --tabColor '#009999' -d C:\git\lazy_guide;
+wt --tabColor '#f59218' -d C:\git\LazyGuide\chapters `; new-tab --tabColor '#009999' -d C:\git\lazy_guide\src\types;
