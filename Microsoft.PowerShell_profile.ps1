c6b3eac0d842da177f8456dc54d009f1b924f0dd
Bruce Eckel <BruceTEckel@gmail.com>
2024-12-11 19:18:24 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -123,3 +123,14 @@ function Set-Location {
 # Check and activate the virtual environment at startup if we start in the desired directory
 $currentPath = Get-Location
 Enable-LazyGuideVenv -PathToCheck $currentPath.Path
+
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
